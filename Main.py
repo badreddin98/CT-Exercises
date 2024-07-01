20496528cb8de29a3dc159ebeffff1bb98021d09
badreddin98 <56277339+badreddin98@users.noreply.github.com>
2024-07-01 19:32:12 -0400

    test

---
+++ Main.py
@@ -0,0 +1,9 @@
+favorite_qoute = """"
+python programing is more that just skill;
+it's a journey into the world of programing-solving and creativity,
+where each line of a code is a step towards mastery.
+"""
+print(favorite_qoute)
+
+sum = 5 + 3 
+print("the sum of 5 and 3 is", sum)
