8c086fe91801bed14922d90330c27c65375fc5b8
badreddin98 <56277339+badreddin98@users.noreply.github.com>
2024-07-02 23:01:14 -0400

    updated file

---
+++ Module2Lesson1.py
@@ -0,0 +1,121 @@
+#exercise1
+fullName = "Badreddin Elazygi"
+print(fullName)
+
+#exercise2
+myName = "Badreddin"
+myname = "Elyazgi"
+print(myName)
+print(myname)
+
+#exercise3
+favColor = "Blue"
+print(favColor)
+
+#exercise4
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+print(fruit1, fruit2, fruit3)
+
+#exercise5
+integerVar = 42
+floatVar = 3.14
+stringVar = "Hello, World"
+print(type(integerVar))
+print(type(floatVar))
+print(type(stringVar))
+
+#exercise6
+mysteryNumber1, mysteryNumber2 = 1234, "1234"
+print(type(mysteryNumber1))
+print(type(mysteryNumber2))
+
+#exercise7
+dynamicVar = 10
+print(type(dynamicVar))
+dynamicVar = "Python is Great"
+print(type(dynamicVar))
+
+#exercise8
+#if = 5
+
+#exercise9
+PI = 3.14159
+print(PI)
+
+result = 2 + 3 * 2 ** 2
+print(result)
+
+#topic4 exercise 1
+flourPerCake = 250
+totalFlour = 2.5 * 1001
+numberOfCakes = totalFlour // flourPerCake
+print(numberOfCakes)
+
+#topic4 exercise 2
+kingdom = "pythonland"
+print(kingdom)
+
+#topic4 exercise 3
+shirt1 = 45
+shirt2 = 50
+result = shirt1 < shirt2
+print(result)
+
+#topic4 exercise 4
+rain = False
+heavyRain = False
+takeUmbrella = rain or heavyRain
+print(takeUmbrella)
+
+#topic4 exercise 5 royal ordeer
+result = 3 + 5 * 2 - 8
+print(result)
+
+#topic4 exercise 6 the pastrey fraction
+pastries = 10
+friends = 3
+pastriesEach = pastries // friends
+print(pastriesEach, "pastries for each friend")
+leftOver = pastries % friends
+print("left over: ", leftOver)
+
+#topic4 exercise 7 kingdom expansion
+kingdom = "Pythonland"
+kingdom += "is wonderful!"
+print(kingdom)
+
+#topic4 exercise 8 royal duel
+night1 = 45
+night2 = 50
+result = night1 == night2
+print(result)
+
+#topic4 exercise 9 chef's special
+eggs = True
+flour = False
+makePancakes = eggs and flour
+print(makePancakes)
+
+#topic4 exercise 10 medieval architecture 
+castleHeight = 100
+moatWidth = 50
+castleHeight *= 2
+moatWidth /= 2
+print(castleHeight, moatWidth)
+
+#topic 5 exercise 1 the forgotten qoute 
+#print("Hello, world!)
+
+#topic 5 exercise 2 the misMatched socks
+num = 7
+if num % 2 != 0:
+  print("the number is odd!")
+else:
+  print("the number is even!")
+
+#topic 5 exercise 3 the overeager greater
+color = "pinkred"
+if color == "red":
+  print("Hello, red!")
+else:
+  print("you are not red!")
